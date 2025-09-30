9a3ef2ce5b3e88a3645ce82d85b452e01c07b289
Domenique412 <dwatkinswise@gmail.com>
2025-09-29 21:22:40 -0400

    styled cards a lil bit

--- se_project_react/src/components/Main/Main.jsx
+++ se_project_react/src/components/Main/Main.jsx
@@ -1,17 +1,22 @@
 import WeatherCard from "../WeatherCard/WeatherCard";
-import { defaultClothingItems } from "../../utils/constants";
 import ItemCard from "../ItemCard/ItemCard";
+import { defaultClothingItems } from "../../utils/constants";
+import "./Main.css";
 
-function Main() {
+function Main({ weatherData }) {
   return (
     <main>
       <WeatherCard />
       <section className="cards">
         <p className="cards__text">Today is 78 &deg; F</p>
         <ul className="cards__list">
-          {defaultClothingItems.map((item) => {
-            return <ItemCard key={item._id} item={item} />;
-          })}
+          {defaultClothingItems
+            .filter((item) => {
+              return item.weather === weatherData.type;
+            })
+            .map((item) => {
+              return <ItemCard key={item._id} item={item} />;
+            })}
         </ul>
       </section>
     </main>
